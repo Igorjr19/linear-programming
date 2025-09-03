e5eba48ba8875954ab53f0d66a805c388dde94c6
Ígor José Rodrigues <igorjr2003@gmail.com>
2025-09-03 13:04:17 -0300

    refactor: enhance logging in simplex_min function and improve parse function structure

--- simplex.py
+++ simplex.py
@@ -1,7 +1,13 @@
 import numpy as np
 from numpy import linalg
 
-def simplex_min(A, b, c, Xn, Xb):
+
+def log(msg, verbose=True):
+    if verbose:
+        print(msg, verbose)
+
+
+def simplex_min(A, b, c, Xn, Xb, verbose=True):
     max_iter = 10
 
     for _ in range(max_iter):
@@ -17,53 +23,56 @@ def simplex_min(A, b, c, Xn, Xb):
         zj_cj = zj_cj.flatten()
         imax = np.argmax(zj_cj)
         if zj_cj[imax] <= 0:
-            print("\n***Optimal solution found!***")
-            print(f"Optimal value: {zBarra.flatten()[0]}")
-            print(f"Basic variables: {Xb}, Non-basic variables: {Xn}")
-            print(f"Solution: {xBarra.flatten()}")
+            log("\n***Optimal solution found!***", verbose)
+            log(f"Optimal value: {zBarra.flatten()[0]}", verbose)
+            log(f"Basic variables: {Xb}, Non-basic variables: {Xn}", verbose)
+            log(f"Solution: {xBarra.flatten()}", verbose)
             return Xb, xBarra, zBarra
-        
+
         yi = B_inv @ A[:, Xn[imax]]
-        
-        with np.errstate(divide='ignore', invalid='ignore'):
+
+        with np.errstate(divide="ignore", invalid="ignore"):
             theta = np.where(yi > 0, xBarra.flatten() / yi.flatten(), np.inf)
-        
+
         if np.all(theta == np.inf):
-            print("Unbounded solution")
+            log("Unbounded solution", verbose)
             return None
-        
+
         imin = np.argmin(theta)
 
         Xb[imin], Xn[imax] = Xn[imax], Xb[imin]
-        print(f"Iteration {_ + 1}: Xb = {Xb}, xBarra = {xBarra.flatten()}, zBarra = {zBarra.flatten()}")
-        
+        log(
+            f"Iteration {_ + 1}: Xb = {Xb}, xBarra = {xBarra.flatten()}, zBarra = {zBarra.flatten()}",
+            verbose,
+        )
 
+    log("Maximum iterations reached without finding optimal solution", verbose)
 
-    print("Maximum iterations reached without finding optimal solution")
-    
     return None
 
+
 def handle_target(c, target):
-    if target == 'max':
+    if target == "max":
         return -c
-    elif target == 'min':
+    elif target == "min":
         return c
     else:
         raise ValueError("Target must be 'max' or 'min'")
 
-def parse(problem, z, target='max'):
-    num_constraints, num_vars = problem.shape[0], problem.shape[1] - 2
+
+def parse(z, constraints, target="max"):
+    num_constraints, num_vars = constraints.shape[0], constraints.shape[1] - 2
 
     A = np.zeros((num_constraints, num_vars + num_constraints))
     b = np.zeros((num_constraints, 1))
     c = np.zeros((num_vars + num_constraints, 1))
 
     for i in range(num_constraints):
-        A[i, :num_vars] = problem[i, :num_vars]
-        b[i] = problem[i, -1]
-        if problem[i, -2] == '<=':
+        A[i, :num_vars] = constraints[i, :num_vars]
+        b[i] = constraints[i, -1]
+        if constraints[i, -2] == "<=":
             A[i, num_vars + i] = 1
-        elif problem[i, -2] == '>=':
+        elif constraints[i, -2] == ">=":
             A[i, num_vars + i] = -1
         else:
             raise ValueError("Inequality must be '<=' or '>='")
@@ -71,21 +80,28 @@ def parse(problem, z, target='max'):
     c[:num_vars] = z[:, None]
     c = handle_target(c, target)
 
-
     Xb = np.array(range(num_vars, num_vars + num_constraints))
     Xn = np.array(range(num_vars))
 
     return A, b, c, Xn, Xb
 
-target = 'max'
-problem = np.array([
-    [1, 1, '<=', 4],
-    [1, 0, '<=', 3],
-    [0, 1, '<=', 2]
-])
-z = np.array([5, 2])
+
+def simplex(z, constraints, target, verbose=True):
+    A, b, c, Xn, Xb = parse(z, constraints, target)
+
+    two_phase_needed = np.any(constraints[:, -2] == ">=") or np.any(b < 0)
+    if two_phase_needed:
+        log("Two-phase method needed. Not implemented.", verbose)
+        return None
+
+    return simplex_min(A, b, c, Xn, Xb, verbose)
 
 
-A, b, c, Xn, Xb = parse(problem, z, target)
+if __name__ == "__main__":
+    target = "max"
+    constraints = np.array(
+        [[1, 1, "<=", 4], [1, 0, "<=", 3], [0, 1, "<=", 2]], dtype=object
+    )
+    z = np.array([5, 2])
 
-simplex_min(A, b, c, Xn, Xb)
+    simplex(z, constraints, target, True)
